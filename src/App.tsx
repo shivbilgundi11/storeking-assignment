c585f497b4fcf6e710a5359f12f567beb0c94a54
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-10-07 15:09:32 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100">
+        <h1>Hello BootStrap</h1>
+
+        <Button variant="primary">Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
